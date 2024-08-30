8637bb2c4523e377e512416cd0a118b268f45604
stzekov <stanislav.tzekov@dxc.com>
2024-08-30 15:43:34 +0300

    Update Paddle class

--- paddle.py
+++ paddle.py
@@ -12,12 +12,11 @@ class Paddle(Turtle):
 		self.penup()
 		self.shapesize(stretch_wid=6, stretch_len=1)
 		self.goto(position)
-		self.x_cor = position[0]
 
 	def move_up(self):
 		y_cor = self.ycor() + 20
-		self.goto(self.x_cor, y_cor)
+		self.goto(self.xcor(), y_cor)
 
 	def move_down(self):
 		y_cor = self.ycor() - 20
-		self.goto(self.x_cor, y_cor)
+		self.goto(self.xcor(), y_cor)
