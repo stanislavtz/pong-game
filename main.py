00de13da6ff8d6c4ddad2e7187397426b9157364
stzekov <stanislav.tzekov@dxc.com>
2024-08-30 17:18:12 +0300

    Update ball collision with paddles

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import time
 from turtle import Screen
 
-import time
-
-from paddle import Paddle
 from ball import Ball
+from paddle import Paddle
 from scoreboard import ScoreBoard
 
 COLOR = "black"
@@ -31,30 +30,30 @@ sc.onkeypress(right_paddle.move_down, "Down")
 
 while not game_is_over:
 	sc.update()
-	time.sleep(ball.speed)
+	time.sleep(ball.move_speed)
 	ball.move()
 
 	# Define collision with wall
 	if ball.ycor() == 380 or ball.ycor() == -380:
 		ball.bounce_from_wall()
 
-	# Define collision with left paddle
-	if ball.distance(left_paddle) < 30 or ball.distance(right_paddle) < 30:
+	# Define collision with paddles
+	if ((ball.distance(left_paddle) < 60 and ball.xcor() < -530) or (
+			ball.distance(right_paddle) < 60 and ball.xcor() > 530)):
 		ball.bounce_from_paddle()
 
 	# Define right paddle scoring and ball reset position
-	if ball.xcor() < -600:
+	if ball.xcor() < -560:
 		ball.reset()
 		score_board.increase_right_score()
 
 	# Define left paddle scoring and ball reset position
-	if ball.xcor() > 600:
+	if ball.xcor() > 560:
 		ball.reset()
 		score_board.increase_left_score()
 
 	# Define winner
 	if score_board.winner():
 		game_is_over = True
-
 
 sc.exitonclick()
