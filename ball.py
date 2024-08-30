20fce303bc0b33ff6d44be7088e510264afbafe6
stzekov <stanislav.tzekov@dxc.com>
2024-08-30 09:55:22 +0300

    Add ball bounce functionalities

--- ball.py
+++ ball.py
@@ -10,8 +10,16 @@ class Ball(Turtle):
 		self.shape(SHAPE)
 		self.color(COLOR)
 		self.penup()
+		self.x_step = 10
+		self.y_step = 10
 
 	def move(self):
-		new_xcor = self.xcor() + 10
-		new_ycor = self.ycor() + 10
+		new_xcor = self.xcor() + self.x_step
+		new_ycor = self.ycor() + self.y_step
 		self.goto(new_xcor, new_ycor)
+
+	def bounce_from_wall(self):
+		self.y_step *= -1
+
+	def bounce_from_paddle(self):
+		self.x_step *= -1
--- main.py
+++ main.py
@@ -28,7 +28,15 @@ sc.onkeypress(right_paddle.move_down, "Down")
 
 while not game_is_over:
 	sc.update()
-	time.sleep(0.05)
+	time.sleep(0.075)
 	ball.move()
+
+	# Define collision with wall
+	if ball.ycor() == 380 or ball.ycor() == -380:
+		ball.bounce_from_wall()
+
+	# Define collision with paddle
+	if ball.distance(left_paddle) < 30 or ball.distance(right_paddle) < 30:
+		ball.bounce_from_paddle()
 
 sc.exitonclick()
