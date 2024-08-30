046a7d26b52ab4a2907700daa7356d3d64b3f63f
stzekov <stanislav.tzekov@dxc.com>
2024-08-30 09:16:43 +0300

    Create Paddle model

---
+++ paddle.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+COLOR = "white"
+SHAPE = "square"
+
+
+class Paddle(Turtle):
+	def __init__(self, position):
+		super().__init__()
+		self.color(COLOR)
+		self.shape(SHAPE)
+		self.penup()
+		self.shapesize(stretch_wid=6, stretch_len=1)
+		self.goto(position)
+		self.x_cor = position[0]
+
+	def move_up(self):
+		y_cor = self.ycor() + 20
+		self.goto(self.x_cor, y_cor)
+
+	def move_down(self):
+		y_cor = self.ycor() - 20
+		self.goto(self.x_cor, y_cor)
